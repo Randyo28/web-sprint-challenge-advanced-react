ac385786843c53383aa45c64fbaf17338e17aa3a
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-12 08:27:32 -0500

    Plants are being displayed and custom hook was used for checkout form

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,30 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react'
+
+const useForm = () => {
+
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+  });
+
+  const handleChanges = (e) => {
+      const {name, value} = e.target
+
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+  return {values, showSuccessMessage, handleChanges, handleSubmit}
+}
+
+export default useForm;
